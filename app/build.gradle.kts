f3b426aad09e6bd628218ff4dbae6639c5edb463
Barra Victor <victor.barra@student.howest.be>
2023-12-29 14:55:22 +0100

    add sdk's and plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -70,6 +71,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     // Add the Firebase dependencies here
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-core:23.0.0")
     implementation("com.google.firebase:firebase-firestore:24.0.0")
 }
