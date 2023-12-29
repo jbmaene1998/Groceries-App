cc21df22d3721dbb8ed0cdcbef2753b1b04816e1
jean-baptiste.maene <jean-baptiste.maene@student.howest.be>
2023-12-29 15:05:44 +0100

    push code

--- app/src/main/java/com/example/thelodge_ai/ScanActivity.kt
+++ app/src/main/java/com/example/thelodge_ai/ScanActivity.kt
@@ -1,24 +1,6 @@
 package com.example.thelodge_ai
 
-import android.os.AsyncTask
-import android.os.Bundle
-import android.util.Log
-import android.view.SurfaceView
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.ImageProxy
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.content.ContextCompat
-import androidx.core.net.toUri
-import java.io.*
-import java.net.HttpURLConnection
-import java.net.URL
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 class ScanActivity : AppCompatActivity() {
 }
