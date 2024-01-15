34585b689091ef1885b77ccd45b221aa4f79ddbf
Barra Victor <victor.barra@student.howest.be>
2024-01-15 11:40:36 +0100

    init ingredients page

---
+++ app/src/main/java/com/example/thelodge_ai/IngredientsActivity.kt
@@ -0,0 +1,19 @@
+package com.example.thelodge_ai
+
+import AppPreferences
+import FirestoreHelper
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class IngredientsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingredients)
+
+    }
+}
